308af4a239d905f04e4babf1dcaca44d0fe46fbe
Esther <esther191299@gmail.com>
2019-11-02 15:48:43 +0100

    added: ffmpeg to run

--- src/openFfmpeg/OpenFfmpeg.java
+++ src/openFfmpeg/OpenFfmpeg.java
@@ -1,4 +1,4 @@
-//If your on windows and press run, kladblok/notepad will open
+
 
 package openFfmpeg;
 
@@ -8,8 +8,12 @@ import java.io.IOException;
 public class OpenFfmpeg {
 
 	public static void main(String[] args) throws IOException {
-		Process open = Runtime.getRuntime().exec("cmd /c notepad");
-
+		
+		/*If your on windows and press run, kladblok/notepad will open*/
+//		Process openNotepad = Runtime.getRuntime().exec("cmd /c notepad");
+		
+		/*If your on windows, have ffmpeg installed and press run, ffmpeg will run*/
+		Process openFFMPEG = Runtime.getRuntime().exec("cmd /c C:\\Users\\Esther PC>ffmpeg");
 	}
 
 }
